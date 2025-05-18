6f1079b28f6b1954582e7ad8c1bb9584951cbd85
SelimhanTokat <selimhantokat16@gmail.com>
2025-05-18 15:59:00 +0300

    Changed the level designs of level 3 and 4.

--- level.py
+++ level.py
@@ -132,57 +132,102 @@ levels = [
             (500, HEIGHT - 40, 100, 40),  # Zemin6
             (600, HEIGHT - 40, 100, 40),  # Zemin7
             (700, HEIGHT - 40, 100, 40),  # Zemin8
-            (100, HEIGHT - 250, 60, 20),  # Platform 1
-            (250, HEIGHT - 250, 60, 20),  # Platform 2
-            (400, HEIGHT - 250, 60, 20),  # Platform 3
-            (550, HEIGHT - 250, 60, 20),  # Platform 4
-            (700, HEIGHT - 250, 60, 20),  # Platform 5
             
-            (150, HEIGHT - 300, 60, 20),  # Platform 6
-            (300, HEIGHT - 300, 60, 20),  # Platform 7
-            (450, HEIGHT - 300, 60, 20),  # Platform 8
-            (600, HEIGHT - 300, 60, 20),  # Platform 9
+            (100, HEIGHT - 150, 60, 30),  # Platform 1            
+            (250, HEIGHT - 150, 60, 20),  # Platform 2            
+            (400, HEIGHT - 150, 60, 30),  # Platform 3            
+            (550, HEIGHT - 150, 60, 20),  # Platform 4            
+            (700, HEIGHT - 150, 60, 30),  # Platform 5
             
-            (50, HEIGHT - 450, 700, 20),  # Platform 10
+            (25, HEIGHT - 150, 60, 30),  # Spike Platform X
+            (175, HEIGHT - 150, 60, 30),  # Spike Platform 1 
+            (325, HEIGHT - 150, 60, 30),  # Spike Platform 2
+            (475, HEIGHT - 150, 60, 30),  # Spike Platform 3
+            (625, HEIGHT - 150, 60, 30),  # Spike Platform 4
+            
+            
+            (100, HEIGHT - 260, 60, 30),  # Spike Platform 6            
+            (250, HEIGHT - 260, 60, 20),  # Spike Platform 7            
+            (400, HEIGHT - 260, 60, 30),  # Spike Platform 8            
+            (550, HEIGHT - 260, 60, 20),  # Spike Platform 9            
+            (700, HEIGHT - 260, 60, 30),  # Spike Platform 10
+            
+            (25, HEIGHT -  260, 60, 20),   # Platform 6
+            (175, HEIGHT - 260, 60, 30),  # Platform 7 
+            (325, HEIGHT - 260, 60, 20),  # Platform 8
+            (475, HEIGHT - 260, 60, 30),  # Platform 9
+            (625, HEIGHT - 260, 60, 20),  # Platform 10
+            
+
+            
+            (100, HEIGHT - 370, 60, 30),  # Spike Platform 6            
+            (250, HEIGHT - 370, 60, 20),  # Spike Platform 7            
+            (400, HEIGHT - 370, 60, 20),  # Spike Platform 8            
+            (550, HEIGHT - 370, 60, 30),  # Spike Platform 9            
+            (700, HEIGHT - 370, 60, 20),  # Spike Platform 10
+            
+            (25, HEIGHT -  370, 60, 20),   # Platform 6
+            (175, HEIGHT - 370, 60, 30),  # Platform 7 
+            (325, HEIGHT - 370, 60, 30),  # Platform 8
+            (475, HEIGHT - 370, 60, 20),  # Platform 9
+            (625, HEIGHT - 370, 60, 30),  # Platform 10
+            
+            (50, HEIGHT - 480, 100,  40),  # Platform 10
+            (150, HEIGHT - 480, 100, 40),  # Platform 11
+            (250, HEIGHT - 480, 100, 40),  # Platform 12
+            (350, HEIGHT - 480, 100, 40),  # Platform 13
+            (450, HEIGHT - 480, 100, 40),  # Platform 14
+            (550, HEIGHT - 480, 100, 40),  # Platform 15
+            (650, HEIGHT - 480, 100, 40),  # Platform 16
+            (750, HEIGHT - 480, 100, 40),  # Platform 17
+            
+            
+            
         ],
         'obstacles': [
-            (175, HEIGHT - 250, 60, 20, 'spikes'),  # Engel 1
-            (325, HEIGHT - 250, 60, 20, 'spikes'),  # Engel 2
-            (475, HEIGHT - 250, 60, 20, 'spikes'),  # Engel 3
-            (625, HEIGHT - 250, 60, 20, 'spikes'),  # Engel 4
+            (40, HEIGHT - 160, 30, 10, 'spikes'),  # Engel X
+            (190, HEIGHT - 160, 30, 10, 'spikes'),  # Engel 1
+            (340, HEIGHT - 160, 30, 10, 'spikes'),  # Engel 2
+            (490, HEIGHT - 160, 30, 10, 'spikes'),  # Engel 3
+            (640, HEIGHT - 160, 30, 10, 'spikes'),  # Engel 4
             
-            (225, HEIGHT - 300, 60, 20, 'spikes'),  # Engel 5
-            (375, HEIGHT - 300, 60, 20, 'spikes'),  # Engel 6
-            (525, HEIGHT - 300, 60, 20, 'spikes'),  # Engel 7
+            (115, HEIGHT - 270, 30, 10, 'spikes'),  # Spike Platform 6            
+            (265, HEIGHT - 270, 30, 10, 'spikes'),  # Spike Platform 7            
+            (415, HEIGHT - 270, 30, 10, 'spikes'),  # Spike Platform 8            
+            (565, HEIGHT - 270, 30, 10, 'spikes'),  # Spike Platform 9            
+            (715, HEIGHT - 270, 30, 10, 'spikes'),  # Spike Platform 10
             
-            # Engelli yol parçaları
-            (150, HEIGHT - 430, 50, 10, 'spikes'),  # Engel 8
-            (250, HEIGHT - 430, 50, 10, 'spikes'),  # Engel 9
-            (350, HEIGHT - 430, 50, 10, 'spikes'),  # Engel 10
-            (450, HEIGHT - 430, 50, 10, 'spikes'),  # Engel 11
-            (550, HEIGHT - 430, 50, 10, 'spikes'),  # Engel 12
-            (650, HEIGHT - 430, 50, 10, 'spikes'),  # Engel 13
+            (40, HEIGHT -  380, 30, 10, 'spikes'),  # Engel X
+            (190, HEIGHT - 380, 30, 10, 'spikes'),  # Engel 1
+            (340, HEIGHT - 380, 30, 10, 'spikes'),  # Engel 2
+            (490, HEIGHT - 380, 30, 10, 'spikes'),  # Engel 3
+            (640, HEIGHT - 380, 30, 10, 'spikes'),  # Engel 4
+            
+            
+            
+            
         ],
         'collectibles': [
-            (130, HEIGHT - 180, "coin"),  # Para 1
-            (280, HEIGHT - 180, "coin"),  # Para 2
-            (430, HEIGHT - 180, "coin"),  # Para 3
-            (580, HEIGHT - 180, "coin"),  # Para 4
-            (730, HEIGHT - 180, "coin"),  # Para 5
+            (123, HEIGHT - 170, "coin"),  # Para 1
+            (273, HEIGHT - 170, "coin"),  # Para 2
+            (423, HEIGHT - 170, "coin"),  # Para 3
+            (573, HEIGHT - 170, "coin"),  # Para 4
+            (723, HEIGHT - 170, "coin"),  # Para 5
             
-            (180, HEIGHT - 330, "coin"),  # Para 6
-            (330, HEIGHT - 330, "coin"),  # Para 7
-            (480, HEIGHT - 330, "coin"),  # Para 8
-            (630, HEIGHT - 330, "coin"),  # Para 9
+            (48,  HEIGHT - 280, "coin"),  # Para 6
+            (198, HEIGHT - 280, "coin"),  # Para 7
+            (348, HEIGHT - 280, "coin"),  # Para 8
+            (498, HEIGHT - 280, "coin"),  # Para 9
+            (648, HEIGHT - 280, "coin"),  # Para 10
             
-            # Hareket gerektiren paralar
-            (200, HEIGHT - 480, "coin"),  # Para 10
-            (300, HEIGHT - 480, "coin"),  # Para 11
-            (400, HEIGHT - 480, "coin"),  # Para 12
-            (500, HEIGHT - 480, "coin"),  # Para 13
-            (600, HEIGHT - 480, "coin"),  # Para 14
+            (123, HEIGHT - 390, "coin"),  # Para 11
+            (273, HEIGHT - 390, "coin"),  # Para 12
+            (423, HEIGHT - 390, "coin"),  # Para 13
+            (573, HEIGHT - 390, "coin"),  # Para 14
+            (723, HEIGHT - 390, "coin"),  # Para 15
+        
         ],
-        'finish_pos': (WIDTH - 50, HEIGHT - 480)
+        'finish_pos': (WIDTH - 50, HEIGHT - 500)
     },
     
     # Level 4 - Ekstrem Zorluk
@@ -197,59 +242,103 @@ levels = [
             (500, HEIGHT - 40, 100, 40),  # Zemin6
             (600, HEIGHT - 40, 100, 40),  # Zemin7
             (700, HEIGHT - 40, 100, 40),  # Zemin8
+            
             # Dikey platform yolu
-            (150, HEIGHT - 120, 50, 20),
-            (250, HEIGHT - 200, 50, 20),
-            (150, HEIGHT - 280, 50, 20),
-            (250, HEIGHT - 360, 50, 20),
-            (150, HEIGHT - 440, 50, 20),
+            (150, HEIGHT - 150, 50, 20),
+            (250, HEIGHT - 260, 50, 20),
+            (150, HEIGHT - 370, 50, 20),
+            (250, HEIGHT - 480, 50, 20),
+            
+            (210, HEIGHT - 205, 30, 10),
+            (210, HEIGHT - 315, 30, 10),
+            (210, HEIGHT - 425, 30, 10),
+            
+            
+            (390, HEIGHT - 280, 30, 10),
+            (390, HEIGHT - 250, 30, 10),
+            (390, HEIGHT - 220, 30, 10),
+            (390, HEIGHT - 190, 30, 10),
+            (390, HEIGHT - 160, 30, 10),
+            (390, HEIGHT - 130, 30, 10),
+            (390, HEIGHT - 100, 30, 10),
+            (390, HEIGHT - 70, 30, 10),
+            
+            
+            (720, HEIGHT - 220, 30, 10),
+            (720, HEIGHT - 190, 30, 10),
+            (720, HEIGHT - 160, 30, 10),
+            (720, HEIGHT - 130, 30, 10),
+            (720, HEIGHT - 100, 30, 10),
+            (720, HEIGHT - 70, 30, 10),
+            
+            
             
             # Hareketli platform izlenimi veren yapı
-            (400, HEIGHT - 150, 40, 10),
-            (460, HEIGHT - 190, 40, 10),
-            (520, HEIGHT - 230, 40, 10),
-            (580, HEIGHT - 270, 40, 10),
-            (640, HEIGHT - 310, 40, 10),
-            (700, HEIGHT - 350, 40, 10),
+            (460, HEIGHT - 190, 50, 30),
+            (520, HEIGHT - 230, 50, 10),
+            (580, HEIGHT - 270, 50, 10),
+            (640, HEIGHT - 310, 50, 30),
+            (580, HEIGHT - 350, 50, 10),
+            (520, HEIGHT - 390, 50, 10),
+            (460, HEIGHT - 430, 50, 30),
             
             # Üst platform
-            (500, 100, 300, 20),
+            (500, HEIGHT- 500, 100, 20),
+            (600, HEIGHT - 500, 100, 20),
+            (700, HEIGHT - 500, 100, 20),
+            
         ],
         'obstacles': [
             # Dikey engelleyiciler
-            (200, HEIGHT - 170, 50, 10, 'spikes'),
-            (200, HEIGHT - 310, 50, 10, 'spikes'),
-            (200, HEIGHT - 410, 50, 10, 'spikes'),
+            (210, HEIGHT - 215, 30, 10, 'spikes'),
+            (210, HEIGHT - 325, 30, 10, 'spikes'),
+            (210, HEIGHT - 435, 30, 10, 'spikes'),
             
-            # Üst platform engelleri
-            (550, 120, 10, 10, 'spikes'),
-            (600, 120, 10, 10, 'spikes'),
-            (650, 120, 10, 10, 'spikes'),
-            (700, 120, 10, 10, 'spikes'),
+
             
             # Düşen tuzaklar
-            (400, HEIGHT - 300, 20, 260, 'spikes'),  # Dikine duvarlar
-            (700, HEIGHT - 250, 20, 210, 'spikes'),
+            (390, HEIGHT - 300, 30, 20, 'spikes'),
+            (390, HEIGHT - 270, 30, 20, 'spikes'),
+            (390, HEIGHT - 240, 30, 20, 'spikes'),
+            (390, HEIGHT - 210, 30, 20, 'spikes'),
+            (390, HEIGHT - 180, 30, 20, 'spikes'),
+            (390, HEIGHT - 150, 30, 20, 'spikes'),
+            (390, HEIGHT - 120, 30, 20, 'spikes'),
+            (390, HEIGHT - 90, 30, 20, 'spikes'),
+            (390, HEIGHT - 60, 30, 20, 'spikes'),
+            
+            
+            (720, HEIGHT - 240, 30, 20, 'spikes'),
+            (720, HEIGHT - 210, 30, 20, 'spikes'),
+            (720, HEIGHT - 180, 30, 20, 'spikes'),
+            (720, HEIGHT - 150, 30, 20, 'spikes'),
+            (720, HEIGHT - 120, 30, 20, 'spikes'),
+            (720, HEIGHT - 90, 30, 20, 'spikes'),
+            (720, HEIGHT - 60, 30, 20, 'spikes'),
             
             # Ölüm bölgesi
-            (420, HEIGHT - 80, 280, 40, 'spikes'),  # Tuzak zemin
+            (420, HEIGHT - 60, 60, 20, 'spikes'),
+            (480, HEIGHT - 60, 60, 20, 'spikes'),
+            (540, HEIGHT - 60, 60, 20, 'spikes'),
+            (600, HEIGHT - 60, 60, 20, 'spikes'),
+            (660, HEIGHT - 60, 60, 20, 'spikes'),
         ],
         'collectibles': [
             # Dikey yoldaki paralar
-            (175, HEIGHT - 150, "coin"),
-            (275, HEIGHT - 230, "coin"),
-            (175, HEIGHT - 310, "coin"),
-            (275, HEIGHT - 390, "coin"),
-            (175, HEIGHT - 470, "coin"),
+            (168, HEIGHT - 170, "coin"),
+            (268, HEIGHT - 280, "coin"),
+            (168, HEIGHT - 390, "coin"),
+            (268, HEIGHT - 500, "coin"),
             
             # Hareketli platformdaki paralar
-            (420, HEIGHT - 180, "coin"),
             (480, HEIGHT - 220, "coin"),
             (540, HEIGHT - 260, "coin"),
             (600, HEIGHT - 300, "coin"),
-                        (660, HEIGHT - 340, "coin"),
-                        (720, HEIGHT - 380, "coin")
-                    ],
-                    'finish_pos': (WIDTH - 50, HEIGHT - 480)
-                }
-            ]
+            (660, HEIGHT - 340, "coin"),
+            (600, HEIGHT - 380, "coin"),
+            (540, HEIGHT - 420, "coin"),
+            (480, HEIGHT - 460, "coin"),
+        ],
+        'finish_pos': (WIDTH - 50, HEIGHT - 520)
+    }
+]
